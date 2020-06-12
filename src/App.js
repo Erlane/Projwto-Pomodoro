8dc06ac37793e57578a4e912a78314201b554303
Erlane Mendes <erlane.mendes31@gmail.com>
2020-06-12 09:35:50 -0300

    my pomodoro

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component { 
+  constructor() {   
+    super()
+    this.state = {
+      time: 1500,
+      timer: false,
+      
+    }
+  }
+
+  startTime = () => {
+   this.timer = setInterval(() =>
+      this.setState({
+       time: this.state.time - 1,
+      }), 
+    1000);
+  }
+  
+  pausedTime = () => {
+    clearInterval(this.startTime);
+      // timer: true,
+  }
+
+  stopTime = () => {
+    clearTimeout(this.startTime);
+  }
+
+ 
+  render() {
+    return (
+    <div className="container">
+        <h1>======== Meu Pomodoro ========</h1>
+        <section>
+          <div className="timePlay" onClick={this.startTime}> PLAY </div>
+          <div className="timePause" onClick={this.pausedTime}> PAUSE </div>
+          <div className="timeStop" onClick={this.stopTime}> STOP </div>
+        </section>
+        <div className="circle">
+          <div className="content">
+              {/* <p className="paragraph">Sessão</p> */}
+    <p className="time">{Math.floor(this.state.time / 60)}:{this.state.time % 60 < 10 ? `0${this.state.time % 60}` : this.state.time % 60}</p>
+          </div>
+        </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
