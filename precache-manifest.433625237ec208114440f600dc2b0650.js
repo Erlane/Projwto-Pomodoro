664787150d813a27a6a3369612a666302c9f15ac
Erlane Mendes <erlane.mendes31@gmail.com>
2020-06-15 14:48:56 -0300

    Updates

---
+++ precache-manifest.433625237ec208114440f600dc2b0650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475d09923ee80038a9a539c734b15b8c",
+    "url": "/Projeto-Pomodoro/index.html"
+  },
+  {
+    "revision": "68b0cc1372beed54beef",
+    "url": "/Projeto-Pomodoro/static/css/main.20e176d1.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/Projeto-Pomodoro/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Projeto-Pomodoro/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b0cc1372beed54beef",
+    "url": "/Projeto-Pomodoro/static/js/main.5f21ba35.chunk.js"
+  },
+  {
+    "revision": "27f648a748ef1856e20d",
+    "url": "/Projeto-Pomodoro/static/js/runtime-main.d444e523.js"
+  }
+]);
